672f261e94a0d5414a868f6bef889a75d0b8ab0a
malex <olmanue@amazon.com>
2024-03-04 14:13:51 -0800

    Clean up neste nvim folder

--- nvim/lua/plugins/mason-lsp.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "williamboman/mason-lspconfig",
-  opts = {
-    ensure_installed = { "tsserver", "volar", "tailwindcss", "lua_ls", "perlnavigator", "ltex", "powershell_es" },
-    automatic_installation = true,
-  },
-}
